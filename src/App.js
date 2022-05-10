664367caf496eb3985865eb2e11b3977978408d6
Priyank Gupta <Genesis3800>
2022-05-10 20:55:25 +0530

    Demo App partially completed

--- src/App.js
+++ src/App.js
@@ -1,53 +1,136 @@
-import "./App.css";
+import React, { useEffect, useState } from 'react';
+import { ethers } from "ethers";
 
 function App() {
+  const [depositValue, setDepositValue] = useState(0);
+  const [greet, setGreet] = useState('');
+  const [greetingValue, setGreetingValue] = useState('');
+  const [balance, setBalance] = useState();
+
+  const provider = new ethers.providers.Web3Provider(window.ethereum)
+  const signer = provider.getSigner()
+  const contractAddress = "0x8e82fF2D5B24650321863062C8649C6c9242338f";
+  const ABI = [
+    {
+      "inputs": [
+        {
+          "internalType": "string",
+          "name": "_greeting",
+          "type": "string"
+        }
+      ],
+      "stateMutability": "nonpayable",
+      "type": "constructor"
+    },
+    {
+      "inputs": [],
+      "name": "deposit",
+      "outputs": [],
+      "stateMutability": "payable",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "greet",
+      "outputs": [
+        {
+          "internalType": "string",
+          "name": "",
+          "type": "string"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "string",
+          "name": "_greeting",
+          "type": "string"
+        }
+      ],
+      "name": "setGreeting",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    }
+  ]
+
+  const contract = new ethers.Contract(contractAddress, ABI, signer);  
+
+  useEffect(() => {
+    const requestAccounts = async () => {
+      await provider.send("eth_requestAccounts", []);
+    }
+    
+    const getGreeting = async () => {
+      const greeting = await contract.greet();
+      setGreet(greeting);
+    }
+
+    const getBalance = async () => {
+      const balance = await provider.getBalance(contractAddress);
+      setBalance(ethers.utils.formatEther(balance));
+    }
+
+    requestAccounts()
+      .catch(console.error)
+    getBalance()
+      .catch(console.error)
+    getGreeting()
+      .catch(console.error)
+  }, [])
+
+  const handleDepositChange = (e) => {
+    setDepositValue(e.target.value)
+  }
+
+  const handleGreetingChange = (e) => {
+    setGreetingValue(e.target.value);
+  }
+
+  const handleDepositSubmit = async (e) => {
+    e.preventDefault();
+    const ethValue = ethers.utils.parseEther(depositValue)
+    const deposit = await contract.deposit({value: ethValue});
+    await deposit.wait();
+    const balance = await provider.getBalance(contractAddress);
+    setBalance(ethers.utils.formatEther(balance));
+  }
+
+  const handleGreetingSubmit = async (e) => {
+    e.preventDefault();
+    await contract.setGreeting(greetingValue)
+    setGreet(greetingValue);
+    setGreetingValue('');
+  }
+
   return (
-    <div class="container">
-      <div class="container">
-        <div class="row">
-          <div class="col">
-            <h3>Greeting</h3>
-            <p>Contract Balance: 0</p>
-          </div>
-          <div class="col">
-            <form>
-              <div class="mb-3">
-                <label for="exampleInputEmail1" class="form-label">
-                  Email address
-                </label>
-                <input
-                  type="email"
-                  class="form-control"
-                  id="exampleInputEmail1"
-                  aria-describedby="emailHelp"
-                />
-                <div id="emailHelp" class="form-text">
-                  We'll never share your email with anyone else.
-                </div>
+    <div className="container">
+      <div className="row mt-5">
+
+        <div className="col">
+          <h3>{greet}</h3>
+          <p>Contract Balance: {balance} Matic</p>
+        </div>
+
+        <div className="col">
+          <div className="mb-3">
+            <h4>Deposit ETH</h4>
+            <form onSubmit={handleDepositSubmit}>
+              <div className="mb-3">
+                <input type="number" className="form-control" placeholder="0" onChange={handleDepositChange} value={depositValue} />
               </div>
-              <div class="mb-3">
-                <label for="exampleInputPassword1" class="form-label">
-                  Password
-                </label>
-                <input
-                  type="password"
-                  class="form-control"
-                  id="exampleInputPassword1"
-                />
+              <button type="submit" className="btn btn-success">Deposit</button>
+            </form>
+
+            <h4 className="mt-3">Change Greeting</h4>
+            <form onSubmit={handleGreetingSubmit}>
+              <div className="mb-3">
+                <input type="text" className="form-control" placeholder="" onChange={handleGreetingChange} value={greetingValue} />
               </div>
-              <div class="mb-3 form-check">
-                <input
-                  type="checkbox"
-                  class="form-check-input"
-                  id="exampleCheck1"
-                />
-                <label class="form-check-label" for="exampleCheck1">
-                  Check me out
-                </label>
-              </div>
-              <button type="submit" class="btn btn-primary">
-                Submit
-              </button>
+              <button type="submit" className="btn btn-dark">Change</button>
             </form>
           </div>
         </div>
